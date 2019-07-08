b8140fd2533efbc30dda5ebee2f36159034734ea
Marcus Holl <marcus.holl@sap.com>
2019-07-08 12:50:25 +0200

    cat the context file

--- vars/xsDeploy.groovy
+++ vars/xsDeploy.groovy
@@ -291,7 +291,7 @@ def executeXSCommand(Map dockerOptions, String command, boolean returnStdout = f
         // the call is properly wrapped by withCredentials(./.)
 
 
-        sh 'ls -la;  whoami; [ -f .xsconfig ]  && cat .xsconfig'
+        sh returnStatus: true script: 'ls -la;  cat .xsconfig'
 
         echo "Executing: '${command}'."
 
