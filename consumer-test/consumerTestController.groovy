82ce8a25e0ad36eb84b20727d74e21a60e797404
Marcus Holl <marcus.holl@sap.com>
2019-10-23 14:44:55 +0200

    TMP: no notifications

--- consumer-test/consumerTestController.groovy
+++ consumer-test/consumerTestController.groovy
@@ -160,34 +160,7 @@ def listTestCaseThreads() {
 }
 
 def notifyGithub(state, description) {
-    println "[INFO] Notifying about state '${state}' for commit '${COMMIT_HASH}'."
-
-    URL url = new URL("https://api.github.com/repos/SAP/jenkins-library/statuses/${COMMIT_HASH}")
-    HttpURLConnection con = (HttpURLConnection) url.openConnection()
-    con.setRequestMethod('POST')
-    con.setRequestProperty("Content-Type", "application/json; utf-8");
-    con.setRequestProperty('User-Agent', 'groovy-script')
-    con.setRequestProperty('Authorization', "token ${System.getenv('INTEGRATION_TEST_VOTING_TOKEN')}")
-
-    def postBody = [
-        state      : state,
-        target_url : System.getenv('TRAVIS_BUILD_WEB_URL'),
-        description: description,
-        context    : "integration-tests"
-    ]
-
-    con.setDoOutput(true)
-    con.getOutputStream().withStream { os ->
-        os.write(toJson(postBody).getBytes("UTF-8"))
-    }
-
-    int responseCode = con.getResponseCode()
-    if (responseCode != HttpURLConnection.HTTP_CREATED) {
-        exitPrematurely("[ERROR] Posting status to github failed. Expected response code " +
-            "'${HttpURLConnection.HTTP_CREATED}', but got '${responseCode}'. " +
-            "Response message: '${con.getResponseMessage()}'",
-            34) // Error code taken from curl: CURLE_HTTP_POST_ERROR
-    }
+    println "[INFO] Notifying about state '${state}' for commit '${COMMIT_HASH}' DISABLED."
 }
 
 def changeDoesNotNeedConsumerTesting() {
