53ee84c6e0ae0e0fbe92e7cca5c452bf492ffed5
Marcus Holl <marcus.holl@sap.com>
2019-10-29 10:43:40 +0100

    wip

--- cmd/xsDeploy.go
+++ cmd/xsDeploy.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"errors"
 	"github.com/SAP/jenkins-library/pkg/command"
+	"os"
 )
 
 
@@ -14,6 +16,27 @@ func xsDeploy(myXsDeployOptions xsDeployOptions) error {
 func runXsDeploy(XsDeployOptions xsDeployOptions, command execRunner) error {
 
 	fmt.Println("[DEBUG] Inside xsDeploy")
+	err := login()
+
+	return err
+}
+
+func login() error {
+	fmt.Println("[DEBUG] inside login")
+	c := command.Command{}
+	c.RunShell("/bin/bash", "echo Hello && touch .xyz")
+
+	if ! fileExists(".xxyz") {
+		return errors.New("File does not exist")
+	}
 
 	return nil
+}
+
+func fileExists(filename string) bool {
+    f, err := os.Stat(filename)
+    if os.IsNotExist(err) {
+        return false
+    }
+    return !f.IsDir()
 }
