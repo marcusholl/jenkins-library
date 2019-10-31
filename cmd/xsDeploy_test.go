c97c9f00e4cad83f8a779bc5e39102ff8730e6f5
Marcus Holl <marcus.holl@sap.com>
2019-10-31 12:13:12 +0100

    prepare for more tests

--- cmd/xsDeploy_test.go
+++ cmd/xsDeploy_test.go
@@ -8,26 +8,30 @@ import (
 
 func TestXSLogin(t *testing.T) {
 
-	s := shellMockRunner{}
-	myXsDeployOptions := xsDeployOptions{
-		APIURL: "https://example.org:12345",
-		User: "me",
-		Password: "secret",
-		Org: "myOrg",
-		Space: "mySpace",
-		LoginOpts: "--skip-ssl-validation",
-		XsSessionFile: ".xs_session",
-	}
+	t.Run("success case", func(t *testing.T) {
+		s := shellMockRunner{}
 
-	e := xsLogin(myXsDeployOptions, &s, func(f string) bool {
-		return f == ".xs_session"
+		myXsDeployOptions := xsDeployOptions{
+			APIURL: "https://example.org:12345",
+			User: "me",
+			Password: "secret",
+			Org: "myOrg",
+			Space: "mySpace",
+			LoginOpts: "--skip-ssl-validation",
+			XsSessionFile: ".xs_session",
+		}
+
+		e := xsLogin(myXsDeployOptions, &s, func(f string) bool {
+			return f == ".xs_session"
+		})
+
+		if e != nil {
+			t.Errorf("XSDeploy command failed: %v", e)
+		}
+
+		cmds := strings.Split(s.calls[0], "\n")
+		assert.Equal(t, cmds[0], "#!/bin/bash")
+		assert.Contains(t, cmds[1], "xs login -a https://example.org:12345 -u me -p 'secret' -o myOrg -s mySpace --skip-ssl-validation")
+		assert.Contains(t, cmds[3], "cp \"${HOME}/.xs_session\" .")
 	})
-	if e != nil {
-		t.Errorf("XSDeploy command failed: %v", e)
-	}
-
-	cmds := strings.Split(s.calls[0], "\n")
-	assert.Equal(t, cmds[0], "#!/bin/bash")
-	assert.Contains(t, cmds[1], "xs login -a https://example.org:12345 -u me -p 'secret' -o myOrg -s mySpace --skip-ssl-validation")
-	assert.Contains(t, cmds[3], "cp \"${HOME}/.xs_session\" .")
 }
