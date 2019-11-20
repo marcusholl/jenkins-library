867dd01ffcbe46d646824284978f7b14a8b8c15e
Marcus Holl <marcus.holl@sap.com>
2019-11-20 13:39:34 +0100

    introduce xs deploy

--- cmd/piper.go
+++ cmd/piper.go
@@ -45,6 +45,7 @@ func Execute() {
 	rootCmd.AddCommand(ConfigCommand())
 	rootCmd.AddCommand(VersionCommand())
 	rootCmd.AddCommand(KarmaExecuteTestsCommand())
+	rootCmd.AddCommand(XsDeployCommand())
 	rootCmd.AddCommand(GithubPublishReleaseCommand())
 
 	addRootFlags(rootCmd)
