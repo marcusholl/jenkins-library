3c90c9bb0ed31f3b09664b011099efc9161289bb
Marcus Holl <marcus.holl@sap.com>
2020-02-19 11:16:43 +0100

    wip: config

--- vars/mtaBuild.groovy
+++ vars/mtaBuild.groovy
@@ -2,11 +2,15 @@ import static com.sap.piper.Prerequisites.checkScript
 
 import com.sap.piper.GenerateDocumentation
 import com.sap.piper.ConfigurationHelper
+import com.sap.piper.GoConfigHelper
 import com.sap.piper.Utils
 import com.sap.piper.PiperGoUtils
 import groovy.transform.Field
 
 @Field def STEP_NAME = getClass().getName()
+@Field String PIPER_DEFAULTS = 'default_pipeline_environment.yml'
+@Field String METADATA_FOLDER = '.pipeline' // metadata file contains already the "metadata" folder level, hence we end up in a folder ".pipeline/metadata"
+@Field String ADDITIONAL_CONFIGS_FOLDER='.pipeline/additionalConfigs'
 
 @Field Set GENERAL_CONFIG_KEYS = []
 @Field Set STEP_CONFIG_KEYS = [
@@ -49,21 +53,39 @@ void call(Map parameters = [:]) {
             .dependingOn('mtaBuildTool').mixin('dockerImage')
             .use()
 
-        dockerExecute(
-            script: script,
-            dockerImage: configuration.dockerImage,
-            dockerEnvVars: configuration.dockerEnvVars,
-            dockerOptions: configuration.dockerOptions,
-            dockerWorkspace: configuration.dockerWorkspace
-        ) {
+        def contextConfig = sh(returnStdout: true, script: "./piper getConfig --stepMetadata '${METADATA_FOLDER}/${METADATA_FILE}' --defaultConfig ${configFiles} --contextConfig")
 
-            sh """#!/bin/bash
-                ./piper mtaBuild"""
+        echo "CONTEXT_CONFIG: ${contextConfig}"
 
-            script.commonPipelineEnvironment.readFromDisk(script)
+//Map contextConfig = readJSON (text: sh(returnStdout: true, script: contextConfigScript))
 
+
+        String configFiles = GoConfigHelper.prepareConfigurations([PIPER_DEFAULTS].plus(script.commonPipelineEnvironment.getCustomDefaults()), ADDITIONAL_CONFIGS_FOLDER)
+
+        writeFile(file: "${METADATA_FOLDER}/${METADATA_FILE}", text: libraryResource(METADATA_FILE))
+
+        def contextConfig = "./piper getConfig --stepMetadata '${METADATA_FOLDER}/${METADATA_FILE}' --defaultConfig ${configFiles} --contextConfig"
+
+        withEnv([
+            "PIPER_parametersJSON=${groovy.json.JsonOutput.toJson(parameters)}",
+        ]) {
+
+            dockerExecute(
+                script: script,
+                dockerImage: configuration.dockerImage,
+                dockerEnvVars: configuration.dockerEnvVars,
+                dockerOptions: configuration.dockerOptions,
+                dockerWorkspace: configuration.dockerWorkspace
+            ) {
+
+                sh """#!/bin/bash
+                    ./piper mtaBuild"""
+
+                script.commonPipelineEnvironment.readFromDisk(script)
+
+            }
+            echo "mtar file created by the build: '${script.commonPipelineEnvironment.mtarFilePath}'"
         }
-        echo "mtar file created by the build: '${script.commonPipelineEnvironment.mtarFilePath}'"
     }
 }
 
