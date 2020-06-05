aa5ddaa6b983b5027531f4de7f3f3f31558fda54
Marcus Holl <marcus.holl@sap.com>
2020-06-05 13:26:25 +0200

    test scripts (do not push)

---
+++ cf-mta-test.sh
@@ -0,0 +1,20 @@
+./piper --verbose \
+    --noTelemetry \
+  cloudFoundryDeploy \
+    --apiEndpoint https://example.org/cf \
+    --deployTool mtaDeployPlugin \
+    --org org \
+    --space space \
+    --username itsme \
+    --password secret \
+    --mtaExtensionDescriptor philipsburg \
+    --apiParameters 'my Api xParameters' \
+    --deployType standard \
+    --smokeTestScript blueGreenCheckScript.sh \
+    --appName hugo \
+    --keepOldInstance true \
+    --manifestVariables a=x \
+    --manifestVariables b=y \
+    --manifestVariablesFiles 123.txt \
+    --manifestVariablesFiles abc.txt \
+    --manifest manifest.yml \
