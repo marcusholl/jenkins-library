96f20095bde24b0884b8e0221dcfd0032086e7f2
Marcus Holl <marcus.holl@sap.com>
2020-08-20 16:18:05 +0200

    fix syntax errors

--- src/com/sap/piper/cm/ChangeManagement.groovy
+++ src/com/sap/piper/cm/ChangeManagement.groovy
@@ -216,10 +216,10 @@ public class ChangeManagement implements Serializable {
             //   password: env:ABAP_PASSWORD
             // )
 
-            dockerEnvVars << [ABAP_USER: username, ABAP_PASSWORD: password]
+            def dockerEnvVars = docker.envVars + [ABAP_USER: username, ABAP_PASSWORD: password]
 
             // when we install globally we need to be root, after preparing that we can su node` in the bash script.
-            dockerOptions = docker.options + '-u 0' // should only be added if not already present.
+            def dockerOptions = docker.options + '-u 0' // should only be added if not already present.
 
             this.script.dockerExecute(
                 script: this.script,
