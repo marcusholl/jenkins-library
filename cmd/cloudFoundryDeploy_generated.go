4b917fed5700992ad64bd2122f2024c41d6c80a9
Marcus Holl <marcus.holl@sap.com>
2020-10-15 13:16:47 +0200

    wip regenerate

--- cmd/cloudFoundryDeploy_generated.go
+++ cmd/cloudFoundryDeploy_generated.go
@@ -35,7 +35,7 @@ type cloudFoundryDeployOptions struct {
 	ManifestVariablesFiles   []string `json:"manifestVariablesFiles,omitempty"`
 	MtaDeployParameters      string   `json:"mtaDeployParameters,omitempty"`
 	MtaExtensionDescriptor   string   `json:"mtaExtensionDescriptor,omitempty"`
-	MtaExtensionCredentials  string   `json:"mtaExtensionCredentials,omitempty"`
+	MtaExtensionCredentials  []string `json:"mtaExtensionCredentials,omitempty"`
 	MtaPath                  string   `json:"mtaPath,omitempty"`
 	Org                      string   `json:"org,omitempty"`
 	Password                 string   `json:"password,omitempty"`
@@ -175,7 +175,7 @@ func addCloudFoundryDeployFlags(cmd *cobra.Command, stepConfig *cloudFoundryDepl
 	cmd.Flags().StringSliceVar(&stepConfig.ManifestVariablesFiles, "manifestVariablesFiles", []string{`manifest-variables.yml`}, "path(s) of the Yaml file(s) containing the variable values to use as a replacement in the manifest file. The order of the files is relevant in case there are conflicting variable names and values within variable files. In such a case, the values of the last file win.")
 	cmd.Flags().StringVar(&stepConfig.MtaDeployParameters, "mtaDeployParameters", `-f`, "Additional parameters passed to mta deployment command")
 	cmd.Flags().StringVar(&stepConfig.MtaExtensionDescriptor, "mtaExtensionDescriptor", os.Getenv("PIPER_mtaExtensionDescriptor"), "Defines additional extension descriptor file for deployment with the mtaDeployPlugin")
-	cmd.Flags().StringVar(&stepConfig.MtaExtensionCredentials, "mtaExtensionCredentials", os.Getenv("PIPER_mtaExtensionCredentials"), "Defines a map of credentials that need to be replaced in the `mtaExtensionDescriptor`. This map needs to be created as `value-to-be-replaced`:`id-of-a-credential-in-jenkins`")
+	cmd.Flags().StringSliceVar(&stepConfig.MtaExtensionCredentials, "mtaExtensionCredentials", []string{}, "Defines a map of credentials that need to be replaced in the `mtaExtensionDescriptor`. This map needs to be created as `value-to-be-replaced`:`id-of-a-credential-in-jenkins`")
 	cmd.Flags().StringVar(&stepConfig.MtaPath, "mtaPath", os.Getenv("PIPER_mtaPath"), "Defines the path to *.mtar for deployment with the mtaDeployPlugin")
 	cmd.Flags().StringVar(&stepConfig.Org, "org", os.Getenv("PIPER_org"), "Cloud Foundry target organization.")
 	cmd.Flags().StringVar(&stepConfig.Password, "password", os.Getenv("PIPER_password"), "Password")
@@ -379,7 +379,7 @@ func cloudFoundryDeployMetadata() config.StepData {
 						Name:        "mtaExtensionCredentials",
 						ResourceRef: []config.ResourceReference{},
 						Scope:       []string{"PARAMETERS", "STAGES", "STEPS", "GENERAL"},
-						Type:        "string",
+						Type:        "[]string",
 						Mandatory:   false,
 						Aliases:     []config.Alias{{Name: "cloudFoundry/mtaExtensionCredentials"}},
 					},
