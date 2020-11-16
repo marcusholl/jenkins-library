00861fdff396628a65e8cf1716ca4f6cc532f04f
Marcus Holl <marcus.holl@sap.com>
2020-11-16 10:11:40 +0100

    wip

--- pkg/transportrequest/upload.go
+++ pkg/transportrequest/upload.go
@@ -13,7 +13,31 @@ type CTS struct {
 }
 
 // Upload ...
-func (cts *CTS)Upload(command command.ExecRunner) error {
-	command.RunExecutable("fiori", []string{"deploy"} ...)
+func (cts *CTS)Upload(command command.ExecRunner, transportRequestID string, abapPackage string, applicationName string) error {
+
+
+	params := []string{
+		"deploy",
+		"-f", // failfast --> provide return code != 0 in case of any failure
+        "-y", // autoconfirm --> no need to press 'y' key in order to confirm the params and trigger the deployment
+	}
+
+	if len(cts.endpoint) > 0 {
+		params = append(params, "-u", cts.endpoint)
+	}
+	if len(cts.client) > 0 {
+		params = append(params, "-l", cts.client)
+	}
+	if len(transportRequestID) > 0 {
+		params = append(params, "-t", transportRequestID)
+	}
+	if len(abapPackage) > 0 {
+		params = append(params, "-p", abapPackage)
+	}
+	if len(applicationName) > 0 {
+		params = append(params, "-n", applicationName)
+	}
+
+	command.RunExecutable("fiori", params ...)
 	return nil
 }
--- pkg/transportrequest/upload_test.go
+++ pkg/transportrequest/upload_test.go
@@ -10,10 +10,18 @@ func TestUploadCTS(t *testing.T) {
 
 	cts := CTS{endpoint: "https://example.org:8080/cts", client: "001", user: "me", password: "******"}
 	cmd := mock.ExecMockRunner{}
-	cts.Upload(&cmd)
+	cts.Upload(&cmd, "12345678", "abapPackage", "appName")
 	assert.Equal(t,
 		[]mock.ExecCall{
-			{Exec: "fiori", Params: []string{"deploy"}},
+			{Exec: "fiori", Params: []string{
+				"deploy",
+				"-f",
+				"-y",
+				"-u", "https://example.org:8080/cts",
+				"-l", "001",
+				"-t", "12345678",
+				"-p", "abapPackage",
+				"-n", "appName"}},
 		},
 		cmd.Calls)
 
