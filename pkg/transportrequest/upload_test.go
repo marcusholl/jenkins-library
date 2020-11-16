3f4874d955a2865fc4621ba5d44b58ed392ab02b
Marcus Holl <marcus.holl@sap.com>
2020-11-16 13:01:25 +0100

    wip

--- pkg/transportrequest/upload_test.go
+++ pkg/transportrequest/upload_test.go
@@ -13,9 +13,10 @@ func TestUploadCTS(t *testing.T) {
 	files = &filesMock
 	defer func() { files = piperutils.Files{} }()
 
+
 	t.Run("all possible values provided", func(t *testing.T) {
-		cts := CTS{endpoint: "https://example.org:8080/cts", client: "001", user: "me", password: "******"}
 		cmd := mock.ExecMockRunner{}
+		cts := CTS{endpoint: "https://example.org:8080/cts", client: "001", user: "me", password: "******"}
 		cts.Upload(&cmd, "12345678", "ui5-deploy.yaml", CTSApp{pack: "abapPackage", name: "appName", desc: "the Desc"})
 		assert.Equal(t,
 			[]mock.ExecCall{
@@ -37,8 +38,8 @@ func TestUploadCTS(t *testing.T) {
 
 	t.Run("all possible values omitted", func(t *testing.T) {
 		// In this case the values are expected inside the fiori deploy config file
-		cts := CTS{endpoint: "", client: "", user: "me", password: "******"}
 		cmd := mock.ExecMockRunner{}
+		cts := CTS{endpoint: "", client: "", user: "me", password: "******"}
 		cts.Upload(&cmd, "12345678", "ui5-deploy.yaml", CTSApp{pack: "", name: "", desc: ""})
 		assert.Equal(t,
 			[]mock.ExecCall{
