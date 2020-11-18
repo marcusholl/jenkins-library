9b693834b5eeb6ca0317cd014d97dcd3d697ac1e
Marcus Holl <marcus.holl@sap.com>
2020-11-18 11:31:53 +0100

    implement the step

--- cmd/helloWorld_test.go
+++ cmd/helloWorld_test.go
@@ -3,6 +3,9 @@ package cmd
 import (
 	"github.com/SAP/jenkins-library/pkg/mock"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"os"
+	"strings"
 	"testing"
 )
 
@@ -25,27 +28,22 @@ func TestRunHelloWorld(t *testing.T) {
 	t.Run("happy path", func(t *testing.T) {
 		// init
 		config := helloWorldOptions{}
+		config.Name = "SAP"
 
 		utils := newHelloWorldTestsUtils()
-		utils.AddFile("file.txt", []byte("dummy content"))
 
 		// test
+		outOut := os.Stdout
+		r, w, _ := os.Pipe()
+		os.Stdout = w
 		err := runHelloWorld(&config, nil, utils)
 
+		w.Close()
+		captured, _ := ioutil.ReadAll(r)
+		os.Stdout = outOut
+
+		assert.Equal(t, "Hello SAP!", strings.TrimSpace(string(captured)))
 		// assert
 		assert.NoError(t, err)
-	})
-
-	t.Run("error path", func(t *testing.T) {
-		// init
-		config := helloWorldOptions{}
-
-		utils := newHelloWorldTestsUtils()
-
-		// test
-		err := runHelloWorld(&config, nil, utils)
-
-		// assert
-		assert.EqualError(t, err, "cannot run without important file")
 	})
 }
