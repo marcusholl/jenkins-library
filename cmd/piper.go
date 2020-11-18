ddda6a45746525c8e85e36c7863f3061e21c5511
Marcus Holl <marcus.holl@sap.com>
2020-11-18 11:26:10 +0100

    Add helloWorld step

--- cmd/piper.go
+++ cmd/piper.go
@@ -118,6 +118,7 @@ func Execute() {
 	rootCmd.AddCommand(CloudFoundryCreateSpaceCommand())
 	rootCmd.AddCommand(CloudFoundryDeleteSpaceCommand())
 	rootCmd.AddCommand(VaultRotateSecretIdCommand())
+	rootCmd.AddCommand(HelloWorldCommand())
 
 	addRootFlags(rootCmd)
 	if err := rootCmd.Execute(); err != nil {
