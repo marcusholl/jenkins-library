4e50b77831e6484335f6611f1c9f58f8ff9c2fbe
Marcus Holl <marcus.holl@sap.com>
2020-12-09 12:01:39 +0100

    git utils required for transport request related steps

--- pkg/transportrequest/gitutils.go
+++ pkg/transportrequest/gitutils.go
@@ -1,29 +1,32 @@
 package transportrequest
 
 import (
-
+	"fmt"
+	"github.com/SAP/jenkins-library/pkg/piperutils"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"regexp"
 )
 
-func func FindLabelsInCommits(commits object.CommitIter, label string) ([]string, error) {
+// FindLabelsInCommits ...
+func FindLabelsInCommits(commits object.CommitIter, label string) ([]string, error) {
 	allLabels := []string{}
 	labelRegex, err := regexp.Compile(fmt.Sprintf("%s: (.*)", label))
 	if err != nil {
-			return []string{}, fmt.Errorf("Cannot extract label: %w", err)
+		return []string{}, fmt.Errorf("Cannot extract label: %w", err)
 	}
 	err = commits.ForEach(func(c *object.Commit) error {
-			fmt.Printf("[MH] Commit ID: '%s'\n[MH] Commit Message: '%s'\n", c.ID(), strings.TrimSpace(c.Message))
-			labels, err := FindLabels(c.Message, labelRegex)
-			if err != nil {
-					return fmt.Errorf("Cannot extract label '%s' from commit '%s':%w", label, c.ID, err)
-			}
-			if len(labels) > 1 {
-					return fmt.Errorf("Found more than one labels (%s) in commit '%s': %s", label, c.ID(), labels)
-			}
-			allLabels = append(allLabels, labels...)
-			return nil
+		labels, err := findLabels(c.Message, labelRegex)
+		if err != nil {
+			return fmt.Errorf("Cannot extract label '%s' from commit '%s':%w", label, c.ID(), err)
+		}
+		if len(labels) > 1 {
+			return fmt.Errorf("Found more than one labels (%s) in commit '%s': %s", label, c.ID(), labels)
+		}
+		allLabels = append(allLabels, labels...)
+		return nil
 	})
 	if err != nil {
-			return []string{}, fmt.Errorf("Cannot extract label: %w", err)
+		return []string{}, fmt.Errorf("Cannot extract label: %w", err)
 	}
 
 	return piperutils.UniqueStrings(allLabels), nil
@@ -32,7 +35,7 @@ func func FindLabelsInCommits(commits object.CommitIter, label string) ([]string
 func findLabels(text string, labelRegex *regexp.Regexp) ([]string, error) {
 	ids := []string{}
 	for _, e := range labelRegex.FindAllStringSubmatch(text, -1) {
-			ids = append(ids, e[1])
+		ids = append(ids, e[1])
 	}
 	return piperutils.UniqueStrings(ids), nil
 }
