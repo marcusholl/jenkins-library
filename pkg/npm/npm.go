c8441f42f074a701aab62f5176c97e2d867ae754
Marcus Holl <marcus.holl@sap.com>
2020-12-16 16:22:15 +0100

    refactor npm

--- pkg/npm/npm.go
+++ pkg/npm/npm.go
@@ -86,7 +86,7 @@ func (exec *Execute) SetNpmRegistries() error {
 
 	var buffer bytes.Buffer
 	execRunner.Stdout(&buffer)
-	err := execRunner.RunExecutable("npm", "config", "get", npmRegistry)
+	err := exec.Execute([]string{"config", "get", npmRegistry})
 	execRunner.Stdout(log.Writer())
 	if err != nil {
 		return err
@@ -99,13 +99,19 @@ func (exec *Execute) SetNpmRegistries() error {
 
 	if exec.Options.DefaultNpmRegistry != "" && registryRequiresConfiguration(preConfiguredRegistry, "https://registry.npmjs.org") {
 		log.Entry().Info("npm registry " + npmRegistry + " was not configured, setting it to " + exec.Options.DefaultNpmRegistry)
-		err = execRunner.RunExecutable("npm", "config", "set", npmRegistry, exec.Options.DefaultNpmRegistry)
+		err = exec.Execute([]string{"config", "set", npmRegistry, exec.Options.DefaultNpmRegistry})
 		if err != nil {
 			return err
 		}
 	}
 
 	return nil
+}
+
+// Execute Executes an npm command
+func (exec *Execute) Execute(args []string) error {
+	log.Entry().Infof("Executing npm with args: %v", args)
+	return exec.Utils.GetExecRunner().RunExecutable("npm", args...)
 }
 
 func registryIsNonEmpty(preConfiguredRegistry string) bool {
@@ -163,7 +169,6 @@ func (exec *Execute) RunScriptsInAllPackages(runScripts []string, runOptions []s
 }
 
 func (exec *Execute) executeScript(packageJSON string, script string, runOptions []string, scriptOptions []string) error {
-	execRunner := exec.Utils.GetExecRunner()
 	oldWorkingDirectory, err := exec.Utils.Getwd()
 	if err != nil {
 		return fmt.Errorf("failed to get current working directory before executing npm scripts: %w", err)
@@ -192,8 +197,7 @@ func (exec *Execute) executeScript(packageJSON string, script string, runOptions
 		npmRunArgs = append(npmRunArgs, "--")
 		npmRunArgs = append(npmRunArgs, scriptOptions...)
 	}
-
-	err = execRunner.RunExecutable("npm", npmRunArgs...)
+	err = exec.Execute(npmRunArgs)
 	if err != nil {
 		return fmt.Errorf("failed to run npm script %s: %w", script, err)
 	}
@@ -305,7 +309,7 @@ func (exec *Execute) install(packageJSON string) error {
 
 	log.Entry().WithField("WorkingDirectory", dir).Info("Running Install")
 	if packageLockExists {
-		err = execRunner.RunExecutable("npm", "ci")
+		err = exec.Execute([]string{"ci"})
 		if err != nil {
 			return err
 		}
@@ -319,7 +323,7 @@ func (exec *Execute) install(packageJSON string) error {
 			"It is recommended to create a `package-lock.json` file by running `npm Install` locally." +
 			" Add this file to your version control. " +
 			"By doing so, the builds of your application become more reliable.")
-		err = execRunner.RunExecutable("npm", "install")
+		err = exec.Execute([]string{"install"})
 		if err != nil {
 			return err
 		}
