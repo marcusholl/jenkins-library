63ebd6da9f8c65e9db51708f93338b33d7ca752b
Marcus Holl <marcus.holl@sap.com>
2021-01-08 09:57:14 +0100

    prune generated code

--- cmd/checkChangeInDevelopment.go
+++ cmd/checkChangeInDevelopment.go
@@ -1,17 +1,13 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/SAP/jenkins-library/pkg/command"
 	"github.com/SAP/jenkins-library/pkg/log"
-	"github.com/SAP/jenkins-library/pkg/piperutils"
 	"github.com/SAP/jenkins-library/pkg/telemetry"
 )
 
 type checkChangeInDevelopmentUtils interface {
 	command.ExecRunner
-
-	FileExists(filename string) (bool, error)
 
 	// Add more methods here, or embed additional interfaces, or remove/replace as required.
 	// The checkChangeInDevelopmentUtils interface should be descriptive of your runtime dependencies,
@@ -21,7 +17,6 @@ type checkChangeInDevelopmentUtils interface {
 
 type checkChangeInDevelopmentUtilsBundle struct {
 	*command.Command
-	*piperutils.Files
 
 	// Embed more structs as necessary to implement methods or interfaces you add to checkChangeInDevelopmentUtils.
 	// Structs embedded in this way must each have a unique set of methods attached.
@@ -32,7 +27,6 @@ type checkChangeInDevelopmentUtilsBundle struct {
 func newCheckChangeInDevelopmentUtils() checkChangeInDevelopmentUtils {
 	utils := checkChangeInDevelopmentUtilsBundle{
 		Command: &command.Command{},
-		Files:   &piperutils.Files{},
 	}
 	// Reroute command output to logging framework
 	utils.Stdout(log.Writer())
@@ -59,20 +53,5 @@ func checkChangeInDevelopment(config checkChangeInDevelopmentOptions, telemetryD
 
 func runCheckChangeInDevelopment(config *checkChangeInDevelopmentOptions, telemetryData *telemetry.CustomData, utils checkChangeInDevelopmentUtils) error {
 	log.Entry().WithField("LogField", "Log field content").Info("This is just a demo for a simple step.")
-
-	// Example of calling methods from external dependencies directly on utils:
-	exists, err := utils.FileExists("file.txt")
-	if err != nil {
-		// It is good practice to set an error category.
-		// Most likely you want to do this at the place where enough context is known.
-		log.SetErrorCategory(log.ErrorConfiguration)
-		// Always wrap non-descriptive errors to enrich them with context for when they appear in the log:
-		return fmt.Errorf("failed to check for important file: %w", err)
-	}
-	if !exists {
-		log.SetErrorCategory(log.ErrorConfiguration)
-		return fmt.Errorf("cannot run without important file")
-	}
-
 	return nil
 }
--- cmd/checkChangeInDevelopment_test.go
+++ cmd/checkChangeInDevelopment_test.go
@@ -2,52 +2,21 @@ package cmd
 
 import (
 	"github.com/SAP/jenkins-library/pkg/mock"
-	"github.com/stretchr/testify/assert"
+	_ "github.com/stretchr/testify/assert"
 	"testing"
 )
 
 type checkChangeInDevelopmentMockUtils struct {
 	*mock.ExecMockRunner
-	*mock.FilesMock
 }
 
 func newCheckChangeInDevelopmentTestsUtils() checkChangeInDevelopmentMockUtils {
 	utils := checkChangeInDevelopmentMockUtils{
 		ExecMockRunner: &mock.ExecMockRunner{},
-		FilesMock:      &mock.FilesMock{},
 	}
 	return utils
 }
 
 func TestRunCheckChangeInDevelopment(t *testing.T) {
 	t.Parallel()
-
-	t.Run("happy path", func(t *testing.T) {
-		t.Parallel()
-		// init
-		config := checkChangeInDevelopmentOptions{}
-
-		utils := newCheckChangeInDevelopmentTestsUtils()
-		utils.AddFile("file.txt", []byte("dummy content"))
-
-		// test
-		err := runCheckChangeInDevelopment(&config, nil, utils)
-
-		// assert
-		assert.NoError(t, err)
-	})
-
-	t.Run("error path", func(t *testing.T) {
-		t.Parallel()
-		// init
-		config := checkChangeInDevelopmentOptions{}
-
-		utils := newCheckChangeInDevelopmentTestsUtils()
-
-		// test
-		err := runCheckChangeInDevelopment(&config, nil, utils)
-
-		// assert
-		assert.EqualError(t, err, "cannot run without important file")
-	})
 }
