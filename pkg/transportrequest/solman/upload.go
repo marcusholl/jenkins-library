0c72df0f89892de666ef908dce0b0a4c14b3891f
Marcus Holl <marcus.holl@sap.com>
2021-01-15 09:31:06 +0100

    add comment funcs should be better in a util package

--- pkg/transportrequest/solman/upload.go
+++ pkg/transportrequest/solman/upload.go
@@ -59,6 +59,8 @@ func (a *SOLMANUploadAction) Perform(fs fileSystem, command command.ExecRunner) 
 	return err
 }
 
+// the next functions should be in some kind of helper ...
+
 // FindEmptyStrings finds empty strings in a struct.
 // in case the stuct contains another struct, also this struct is checked.
 func FindEmptyStrings(v interface{}) ([]string, error) {
